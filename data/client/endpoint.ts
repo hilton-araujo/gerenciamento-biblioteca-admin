9701e917f9027f68d000832284285d79f86a10e8
Hilton Araujo <hiltoneduardoaraujo@gmail.com>
2024-12-26 10:33:35 +0200

    feat: endpoints crud cliente, categorias e livros

--- data/client/endpoint.ts
+++ data/client/endpoint.ts
@@ -3,15 +3,22 @@ export const API_ENDPOINTS = {
     //GET SEM PARAMETROS
     GET_BOOKS: "/book",
     GET_CATEGORIES: "/category",
+    GET_CLIENTS: "/client",
 
-    //GET COM PARAMETROS\
+    //GET COM PARAMETROS
     GET_BOOK_BY_ID: (id: string) => `/book/${id}`,
     GET_CATEGORY_BY_ID: (code: string) => `/category/${code}`,
+    GET_CLIENT_BY_CODE: (code: string) => `/client/${code}`,
 
     //POST
     ADD_BOOK: `/book`,
     LOGIN: `/login`,
     ADD_CATEGORIES: `/category`,
+    ADD_CLIENT: `/client`,
 
     //PUT
+    EDI_CATEGORY: `/category`,
+    EDIT_CLIENT: `/client`,
+    EDIT_BOOK: `/book`
+
 }
