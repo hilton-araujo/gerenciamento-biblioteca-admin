7290c8488d37ca064aedbe97048b81b78b9af866
Hilton Araujo <hiltoneduardoaraujo@gmail.com>
2025-03-03 10:03:56 +0200

    fix: fix build error

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
-import { withContentlayer } from "next-contentlayer";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
     COOKIE_KEY: process.env.COOKIE_KEY
   }
 };
 
-export default withContentlayer(nextConfig);
+export default nextConfig;
