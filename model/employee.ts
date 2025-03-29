10ac937b5a14f3d18a20b802069d893b92cc61c1
Hilton Araujo <hiltoneduardoaraujo@gmail.com>
2025-03-29 23:13:43 +0200

    feat: adicionar interface Employee para definição de funcionários no sistema

---
+++ model/employee.ts
@@ -0,0 +1,13 @@
+export interface Employee {
+    code: string;
+    name: string;
+    surname: string;
+    genre: string;
+    msidn: string;
+    nuit: string;
+    documentType: string;
+    documentNumber: string;
+    email: string;
+    position: string;
+    active: boolean;
+}
