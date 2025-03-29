327cffc802388a56dbcc4a18fc44454c0dbaf339
Hilton Araujo <hiltoneduardoaraujo@gmail.com>
2025-03-29 20:29:15 +0200

    feat: adicionar suporte para modo de edição no componente de fluxo de trabalho

--- components/features/workflow/addWorkFlow.tsx
+++ components/features/workflow/addWorkFlow.tsx
@@ -1,31 +1,38 @@
-import { TextField } from '@mui/material'
-import { ReloadIcon } from '@radix-ui/react-icons'
-import { FormikProps } from 'formik'
-import React from 'react'
-import { CardContent } from '@/components/shared/ui/card'
-import { generateFieldProps } from '@/components/shared/generateFieldProps'
-import { Button } from '@/components/shared/ui/button'
+"use client"
+
+import { TextField } from "@mui/material"
+import { ReloadIcon } from "@radix-ui/react-icons"
+import type { FormikProps } from "formik"
+import { CardContent } from "@/components/shared/ui/card"
+import { generateFieldProps } from "@/components/shared/generateFieldProps"
+import { Button } from "@/components/shared/ui/button"
 
 interface Props {
     formik: FormikProps<any>
     isPending: boolean
+    isEditMode?: boolean
 }
 
-const AddWorkFlow = ({ formik, isPending }: Props) => {
+const AddWorkFlow = ({ formik, isPending, isEditMode = false }: Props) => {
     return (
         <div>
-            <form>
-                <CardContent>
-                    <div className="flex flex-col gap-2">
-                        <TextField {...generateFieldProps(formik, 'designation', 'Designação', 'Digite a designação do workflow')} />
-                        <TextField {...generateFieldProps(formik, 'description', 'Descrição', 'Digite a descrição do workflow')} />
-                    </div>
-                </CardContent>
+            <form onSubmit={formik.handleSubmit}>
+                <div className="w-full p-2 mt-2 rounded-md shadow-md bg-gray-50">
+                    <fieldset className="h-full p-2 space-y-4 bg-white border rounded-lg shadow-md">
+                        <legend className="text-lg font-medium ">Dados de Workflow</legend>
+                        <div className="flex flex-col gap-2">
+                            <TextField
+                                {...generateFieldProps(formik, "designation", "Designação", "Digite a designação do workflow")}
+                            />
+                            <TextField {...generateFieldProps(formik, "description", "Descrição", "Digite a descrição do workflow")} />
+                        </div>
+                    </fieldset>
+                </div>
             </form>
             <div className="flex items-center justify-end gap-2 mt-2">
                 <Button type="submit" onClick={formik.submitForm} disabled={!formik.isValid || isPending}>
                     {isPending && <ReloadIcon className="mr-2 animate-spin" />}
-                    ADICIONAR
+                    {isEditMode ? "EDITAR" : "ADICIONAR"}
                 </Button>
             </div>
         </div>
