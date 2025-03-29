f3f3bbb1552a49dec85f006de4dae515d91a1f9b
Hilton Araujo <hiltoneduardoaraujo@gmail.com>
2025-03-29 22:04:09 +0200

    feat: substituir componente WorkflowComp por StageComp na aba ETAPA

--- app/admin/workflow/page.tsx
+++ app/admin/workflow/page.tsx
@@ -3,12 +3,13 @@ import React from "react";
 import WorkflowComp from "@/components/features/workflow/workflow";
 import { Plus } from "lucide-react";
 import { TabLayout } from "@/components/common/tabl-layout";
+import StageComp from "@/components/features/workflow/stage/stage";
 
 export default function Workflow() {
 
     const tabs = [
         { label: "FLUXO DE TRABALHO", value: "1", component: <WorkflowComp /> },
-        { label: "ETAPA", value: "2", component: <WorkflowComp /> },
+        { label: "ETAPA", value: "2", component: <StageComp /> },
         { label: "TAREFA", value: "3", component: <WorkflowComp /> },
     ]
 
