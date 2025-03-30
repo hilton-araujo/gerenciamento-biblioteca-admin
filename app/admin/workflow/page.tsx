a54990ce68b574ba00165581644d0d74f1d9380e
Hilton Araujo <hiltoneduardoaraujo@gmail.com>
2025-03-30 13:02:28 +0200

    feat: substituir componente de tarefa no fluxo de trabalho

--- app/admin/workflow/page.tsx
+++ app/admin/workflow/page.tsx
@@ -4,13 +4,14 @@ import WorkflowComp from "@/components/features/workflow/workflow";
 import { Plus } from "lucide-react";
 import { TabLayout } from "@/components/common/tabl-layout";
 import StageComp from "@/components/features/workflow/stage/stage";
+import TaskComp from "@/components/features/workflow/task/task";
 
 export default function Workflow() {
 
     const tabs = [
         { label: "FLUXO DE TRABALHO", value: "1", component: <WorkflowComp /> },
         { label: "ETAPA", value: "2", component: <StageComp /> },
-        { label: "TAREFA", value: "3", component: <WorkflowComp /> },
+        { label: "TAREFA", value: "3", component: <TaskComp /> },
     ]
 
     return <TabLayout title={"Criar Fluxo de Trabalho"} tabs={tabs || []} description={"Aqui vocês pode criar um fluxo de trabalho."} icon={Plus} />;
